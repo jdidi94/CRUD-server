b2b943285e8804daf2a43db0a3cea969741e3498
jdidi94 <jdididaoud1994@gmail.com>
2024-08-07 17:01:44 +0100

    done

--- database/blog.js
+++ database/blog.js
@@ -1,11 +1,14 @@
 const { mongoose, Schema } = require("mongoose");
 
-let blogSchema = mongoose.Schema({
-  imageUrl: String,
-  body: String,
-  title: String,
-  createdAt: String,
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+let blogSchema = mongoose.Schema(
+  {
+    imageUrl: String,
+    body: String,
+    title: String,
+    createdAt: String,
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Repo", blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
